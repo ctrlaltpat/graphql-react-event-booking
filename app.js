fa8f4421be038f5f71140225952c6ff7d549948a
Patrick <ctrlaltpat@gmail.com>
2019-01-21 23:02:49 +0000

    basic test with global object for storage

--- app.js
+++ app.js
@@ -5,15 +5,30 @@ const { buildSchema } = require('graphql')
 
 const app = express()
 
+const events = [];
+
 app.use(bodyParser.json())
 
 app.use('/graphql', graphqlHttp({
   schema: buildSchema(`
+    type Event {
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+    input EventInput {
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
     type RootQuery {
-      events: [String!]!
+      events: [Event!]!
     }
     type RootMutation {
-      createEvent(name: String): String
+      createEvent(eventInput: EventInput): Event
     }
     schema {
       query: RootQuery
@@ -22,11 +37,18 @@ app.use('/graphql', graphqlHttp({
   `),
   rootValue: {
     events: () => {
-      return ['Learn GraphQL', 'Join the gym.... eventually', 'Reconfigure dev env' ]
+      return events
     },
-    createEvent: (args) => {
-      const eventName = args.name;
-      return eventName;
+    createEvent: ({eventInput}) => {
+      const event = {
+        _id: Math.random().toString(),
+        title: eventInput.title,
+        description: eventInput.description,
+        price: +eventInput.price,
+        date: eventInput.date //new Date().toISOString()
+      }
+      events.push(event)
+      return event
     }
   },
   graphiql: true
