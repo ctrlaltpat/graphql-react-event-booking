da7a8b74552f4a4e74d85ab8d1536db65b181c27
Patrick <ctrlaltpat@gmail.com>
2019-01-21 20:28:58 +0000

    very basic setup with schema and resolvers

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const graphqlHttp = require('express-graphql')
+const { buildSchema } = require('graphql')
 
 const app = express()
 
 app.use(bodyParser.json())
 
-app.get('/', (req, res, next) => {
-  res.send('oh hai there')
-})
+app.use('/graphql', graphqlHttp({
+  schema: buildSchema(`
+    type RootQuery {
+      events: [String!]!
+    }
+    type RootMutation {
+      createEvent(name: String): String
+    }
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+  rootValue: {
+    events: () => {
+      return ['Learn GraphQL', 'Join the gym.... eventually', 'Reconfigure dev env' ]
+    },
+    createEvent: (args) => {
+      const eventName = args.name;
+      return eventName;
+    }
+  },
+  graphiql: true
+}))
 
 app.listen(4000)
