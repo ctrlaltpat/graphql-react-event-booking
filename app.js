679cf0401cef1f13c41933ab53ca4127407ed579
Patrick <ctrlaltpat@gmail.com>
2019-01-23 23:39:23 +0000

    successfully creating and retrieving events in mongodb (on atlas) using graphql mutations and queries

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const graphqlHttp = require('express-graphql')
 const { buildSchema } = require('graphql')
 const mongoose = require('mongoose')
 
-const app = express()
+const Event = require('./models/event')
 
-const events = [];
+const app = express()
 
 app.use(bodyParser.json())
 
@@ -38,26 +38,61 @@ app.use('/graphql', graphqlHttp({
   `),
   rootValue: {
     events: () => {
-      return events
+      return Event.find()
+      .then(events => {
+        return events.map(event => {
+          return {...event._doc, _id: event._doc._id.toString()}
+        })
+      }).catch(err => {
+        throw err
+      })
     },
     createEvent: ({eventInput}) => {
-      const event = {
-        _id: Math.random().toString(),
+      const event = new Event({
         title: eventInput.title,
         description: eventInput.description,
         price: +eventInput.price,
-        date: eventInput.date //new Date().toISOString()
-      }
-      events.push(event)
+        date: new Date(eventInput.date)
+      })
       return event
+        .save()
+        .then(result => {
+          console.log(result)
+          return {...result._doc, _id: result._doc._id.toString()} // leave out metadata
+        }).catch(err => {
+          console.log(err)
+          throw err
+        })
     }
   },
   graphiql: true
 }))
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-ixnhp.mongodb.net/test?retryWrites=true`)
-                      .then(() => {
-                        app.listen(4000)
-                      }).catch(err => {
-                        console.log(err)
-                      })
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-ixnhp.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`)
+  .then(() => {
+    app.listen(4000)
+  }).catch(err => {
+    console.log(err)
+  })
+
+
+
+// query {
+//   events {
+//     _id
+//     title
+//     description
+//   }
+// }
+
+// # mutation {
+// #   createEvent(eventInput: {
+// #     title:"A title",
+// #     description:"This works... omg",
+// #     price: 29.99,
+// #     date: "2019-01-21T22:51:07.782Z"
+// #   }) {
+// #     title
+// #     description
+// #   }
+// # }
