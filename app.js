746f9d7d013bd1707b6fcaf953998a131d7d7938
Patrick <ctrlaltpat@gmail.com>
2019-01-30 22:55:46 +0000

    can now create a user (if email isn't already taken)

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const bodyParser = require('body-parser')
 const graphqlHttp = require('express-graphql')
 const { buildSchema } = require('graphql')
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const Event = require('./models/event')
+const User = require('./models/user')
 
 const app = express()
 
@@ -19,17 +21,27 @@ app.use('/graphql', graphqlHttp({
       price: Float!
       date: String!
     }
+    type User {
+      _id: ID!
+      email: String!
+      password: String
+    }
     input EventInput {
       title: String!
       description: String!
       price: Float!
       date: String!
     }
+    input UserInput {
+      email: String!
+      password: String
+    }
     type RootQuery {
       events: [Event!]!
     }
     type RootMutation {
       createEvent(eventInput: EventInput): Event
+      createUser(userInput: UserInput): User
     }
     schema {
       query: RootQuery
@@ -62,6 +74,28 @@ app.use('/graphql', graphqlHttp({
         }).catch(err => {
           console.log(err)
           throw err
+        })
+    },
+    createUser: args => {
+      return User.findOne({email: args.userInput.email})
+        .then(user => {
+          if (user) {
+            throw new Error('User already exists.')
+          }
+          return bcrypt.hash(args.userInput.password, 12)
+        })
+        .then(hashedPassword => {
+          const user = new User({
+            email: args.userInput.email,
+            password: hashedPassword
+          })
+          return user.save()
+        })
+        .then(result => {
+          return {...result._doc, password: null, _id: result.id}
+        })
+        .catch(err => {
+          throw err;
         })
     }
   },
